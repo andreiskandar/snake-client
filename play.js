a53f55b4fab85059e4e03d603c61dbd3e1838e76
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-26 14:10:29 -0700

    added name once connected

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
-const net = require('net');
+const connect = require('./client');
 
-const connect = () => {
-	const conn = net.createConnection({
-		host: '10.0.2.15',
-		port: 50541,
-	});
+console.log('Connecting...');
 
-	conn.setEncoding('utf8');
-
-	// detecting incoming data from the server then log the data
-	conn.on('data', data => {
-		console.log('you ded cuz you idled');
-	});
-	return conn;
-};
-
-console.log('Connecting ...');
 connect();
